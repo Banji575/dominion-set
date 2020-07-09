67a4b8e5937beaff127be396fd5c32587759d1b2
Sergey Churkov <Hotmerch@hotmail.com>
2020-07-09 16:29:04 +0300

    Updates

---
+++ precache-manifest.eacb54e2ab5489da0aa61c6653abcdd5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d0810776b2f97647973cf85ccdf2a55",
+    "url": "/dominion-set/index.html"
+  },
+  {
+    "revision": "cd9b3e89b32718f10141",
+    "url": "/dominion-set/static/css/main.2825a40a.chunk.css"
+  },
+  {
+    "revision": "f9cd8c410c688f149b43",
+    "url": "/dominion-set/static/js/2.478dc2d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dominion-set/static/js/2.478dc2d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd9b3e89b32718f10141",
+    "url": "/dominion-set/static/js/main.04d35313.chunk.js"
+  },
+  {
+    "revision": "486565ed848077f3f965",
+    "url": "/dominion-set/static/js/runtime-main.003d3197.js"
+  }
+]);
